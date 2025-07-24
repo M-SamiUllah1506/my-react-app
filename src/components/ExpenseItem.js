24b348a46bf998e677dc8cdbeaaf016b9736a8b4
MST-WS-SE-014 <mstintern2@gmail.com>
2025-07-24 17:41:53 +0500

    done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,53 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const [price, setPrice] = useState(props.amount);
+  const [isEditing, setIsEditing] = useState(false);
 
-  const [title,setTitle]=useState(props.title)
-const ClickHandler=()=>
-{
-   setTitle('Updated!')
-}
+  const handleEditClick = () => setIsEditing(true);
+  const handleSaveClick = () => setIsEditing(false);
+  const handleTitleChange = (event) => setTitle(event.target.value);
+  const handlePriceChange = (event) => setPrice(event.target.value);
 
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+    <Card className='expense-item full-inline'>
+      <div className='expense-date-inline'>
+        <ExpenseDate date={props.date} />
       </div>
-      <button onClick={ClickHandler}>Change</button>
+
+      {isEditing ? (
+        <>
+          <input
+            type='text'
+            value={title}
+            onChange={handleTitleChange}
+            className='edit-input'
+            placeholder='Enter title'
+          />
+          <input
+            type='number'
+            value={price}
+            onChange={handlePriceChange}
+            className='edit-input'
+            placeholder='Enter price'
+          />
+          <button onClick={handleSaveClick} className='edit-btn'>
+            Save
+          </button>
+        </>
+      ) : (
+        <>
+          <h2 className='expense-title'>{title}</h2>
+          <div className='expense-item__price'>${price}</div>
+          <button onClick={handleEditClick} className='edit-btn'>
+            Edit
+          </button>
+        </>
+      )}
     </Card>
   );
 }
